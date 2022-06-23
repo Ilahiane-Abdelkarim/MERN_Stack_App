6e445f560a70cd481862a31050bda1156b288e96
Ilahiane-Abdelkarim <ilahianeabdelkarim@gmail.com>
2022-06-23 20:01:21 +0100

    . . .

---
+++ server/models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            require: [true, 'Please add a first name'],
+        },
+        last_name: {
+            type: String,
+            require: [true, 'Please add a last name'],
+        },
+        email: {
+            type: String,
+            require: [true, 'Please add a email'],
+            unique: true
+        },
+        password: {
+            type: String,
+            require: [true, 'Please add a password'],
+        },
+        location: {
+            type: String,
+            require: [true, 'Please add a location'],
+        },
+        type: {
+            type: String,
+            require: [true, 'Please add a type'],
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('User', userSchema)
