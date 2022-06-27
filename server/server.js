0a68ea99432f74b77c7bdfe772fa81e4658ad437
Ilahiane-Abdelkarim <ilahianeabdelkarim@gmail.com>
2022-06-27 11:15:56 +0100

    make it better

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+require("dotenv").config()
 
 connectDB()
 
@@ -18,12 +19,12 @@ app.use('/api/users', require('./routes/usersRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__filename, '../client/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => res.send('set env development to production...'))
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__filename, '../client/build')))
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => res.send('set env development to production...'))
+// }
 
 app.use(errorHandler);
 
