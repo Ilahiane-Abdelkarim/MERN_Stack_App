8503e1fe4d3cd6b4f333af546c956b5edb8d4631
Ilahiane-Abdelkarim <ilahianeabdelkarim@gmail.com>
2022-06-27 13:56:51 +0100

    . . .

--- client/src/features/goals/goalService.js
+++ client/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/goals/'
+const API_URL = 'https://ila-test1.herokuapp.com/api/goals/'
 
 // create goal
 const createGoal = async (goalData, token) => {
--- server/server.js
+++ server/server.js
@@ -5,12 +5,15 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 require("dotenv").config()
 
 connectDB()
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://ilahiane.tech'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
